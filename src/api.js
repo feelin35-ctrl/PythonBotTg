ffd20d2f3ba5c676b49e89e9837f9002d96ae97b
feelin35-ctrl <feelin35@gmail.com>
2025-09-21 17:03:15 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import { API_BASE_URL } from './config';
 // Создаем экземпляр axios с базовой конфигурацией
 const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 30000,
 });
 
 export default api;
